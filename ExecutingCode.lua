ad05c4e0d9b164161069cf104548f0faf4e3665a
Sunny <minecraftperm2013@gmail.com>
2025-05-06 14:23:40 +0330

    Update ExecutingCode.lua

--- ExecutingCode.lua
+++ ExecutingCode.lua
@@ -1,1 +1,8 @@
 loadstring(game:HttpGet("https://pastefy.app/0wFc1g6n/raw"))()
+local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+
